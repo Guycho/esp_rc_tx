493c07e9969cf4b89160b96ba3b5b372291c048c
Guy Raiz <guy@verobotics.com>
2025-01-08 15:39:15 +0200

    remove many unneeded files

--- include/transceiver.h
+++ include/transceiver.h
@@ -4,13 +4,11 @@
 #include <ArduinoJson.h>
 #include <WiFi.h>
 #include <Chrono.h>
-#include "control.h"
 #include "ESP_now_handler.h"
 
 
 struct TransceiverConfig {
     uint16_t update_delay_ms;
-    Control *control;
     ESPNowHandler *esp_now_handler;
 };
 
@@ -25,13 +23,9 @@ class Transceiver {
    private:
     float two_decimals(float value);
     Chrono m_data_timer;
-    Control *m_control;
     ESPNowHandler *m_esp_now_handler;
 
-
     uint16_t m_update_delay_ms;
-    float m_max_g_force = 5.0;
-    uint16_t m_max_rpm = 5000;
 };
 
 #endif  // ESP32SERVER_HPP
