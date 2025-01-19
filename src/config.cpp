706b3cafbb69734e7aaf09941ca55d5291b5c458
Guy Raiz <guy@verobotics.com>
2025-01-19 09:53:14 +0200

    remove file for config

--- src/config.cpp
+++ src/config.cpp
@@ -1,19 +1,9 @@
 #include "config.h"
 
 namespace Config {
-namespace Server {
-const char *ssid = "Guycho";
-const char *password = "0544904809";
-// const char *ssid = "OmerRobotics";
-// const char *password = "qazwsx123";
-}  // namespace Server
 namespace Transceiver {
 const uint16_t update_delay_ms = 100;
 }  // namespace Transceiver
-namespace PS4Controller {
-    const char *mac = "A0:DD:6C:03:9A:ED";
-    const float dead_band = 5;
-}  // namespace PS4Controller
 namespace ESPNow {
 const uint8_t peer_mac_address[] = {0xA0, 0xDD, 0x6C, 0x04, 0x2D, 0x98};
 const bool use_lr = true;
