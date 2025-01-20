0b61370949daf6f6fb27b54b87e0fb6a3e98bf5a
Guy Raiz <guy@verobotics.com>
2025-01-20 15:24:55 +0200

    fix pin numbers

--- include/config.h
+++ include/config.h
@@ -23,17 +23,16 @@ enum ButtonName {
 enum PotentiometerName { THROTTLE = 0, STEERING = 1 };
 const uint16_t debounce_delay_ms = 50;
 const uint8_t num_buttons = 13;
-const uint8_t buttons_pins[num_buttons] = {23, 22, 12, 13, 21, 19, 18, 5, 17, 16, 4, 2, 15};
+const uint8_t buttons_pins[num_buttons] = {32, 33, 25, 26, 27, 14, 12, 13, 23, 22, 21, 19, 18};
 const bool buttons_on_states[num_buttons] = {false, false, false, false, false, false, false, false,
   false, false, false, false, false};
 const float potentiometers_dead_band = 5.0;
 const double max_output_value = 100.0;
 const uint8_t num_potentiometers = 2;
 const uint8_t analog_read_resolution = 12;
-const uint8_t potentiometers_pins[num_potentiometers] = {36, 39};
+const uint8_t potentiometers_pins[num_potentiometers] = {34, 35};
 const uint16_t potentiometers_min_input_values[num_potentiometers] = {0, 0};
-const uint16_t potentiometers_max_input_values[num_potentiometers] = {
-  uint16_t(pow(2, analog_read_resolution) - 1), uint16_t(pow(2, analog_read_resolution)- 1)};
+const uint16_t potentiometers_max_input_values[num_potentiometers] = {4095, 4095};
 const bool potentiometers_reverse[num_potentiometers] = {false, false};
 const float potentiometers_mid_pct_input_values[num_potentiometers] = {0.0, 0.0};
 namespace Transceiver {
