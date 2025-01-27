b28dd27dd7cdaa71f278a65ab498a90cb87b6a50
GuyRaiz <guy@verobotics.com>
2025-01-27 20:50:27 +0200

    enable lr

--- include/config.h
+++ include/config.h
@@ -42,7 +42,7 @@ const uint16_t update_delay_ms = 25;
 }  // namespace Transceiver
 namespace ESPNow {
 const uint8_t peer_mac_address[] = {0xA0, 0xDD, 0x6C, 0x04, 0x2D, 0x98};
-const bool use_lr = false;
+const bool use_lr = true;
 const bool print_debug = false;
 }  // namespace ESPNow
 namespace OTAHandler {
