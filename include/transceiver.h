45ed7e18d4a88163cce8007868bb1bb5b7dd172b
Guy Raiz <guy@verobotics.com>
2025-01-29 09:20:09 +0200

    update recieving data

--- include/transceiver.h
+++ include/transceiver.h
@@ -9,18 +9,19 @@
 #include "config.h"
 #include "input.h"
 
+
+struct TelemetryData {
+    bool arm_state;
+    uint8_t steering_mode;
+    uint8_t drive_mode;
+    uint8_t battery_status;
+};
+
 struct TransceiverConfig {
     InputController *input_controller;
     ESPNowHandler *esp_now_handler;
 
     uint16_t update_delay_ms;
-};
-
-struct RemoteData {
-    bool arm_state;
-    uint8_t steering_mode;
-    uint8_t drive_mode;
-    uint8_t battery_status;
 };
 
 class Transceiver {
@@ -30,20 +31,20 @@ class Transceiver {
 
     void init(const TransceiverConfig &config);
     void run();
+    TelemetryData get_telemetry_data();
 
    private:
     ESPNowHandler *m_esp_now_handler;
     InputController *m_input_controller;
 
     void send_data();
-    String get_remote_data();
     bool verify_checksum(const String &data);
-    RemoteData parse_remote_data(const String &data);
-
+    TelemetryData parse_remote_data(const String &data);
+    void update_ui();    
     Chrono m_data_timer;
 
     InputControllerData m_input_controller_data;
-    RemoteData m_remote_data;
+    TelemetryData m_telemetry_data;
     String m_remote_data;
     uint16_t m_update_delay_ms;
     uint8_t m_data_index = 0;
--- src/transceiver.cpp
+++ src/transceiver.cpp
@@ -15,9 +15,11 @@ void Transceiver::run() {
     if (!m_data_timer.hasPassed(m_update_delay_ms, true)) {
         return;
     }
-    m_input_controller_data = m_input_controller->get_data();
-    send_data();
     m_remote_data = m_esp_now_handler->get_data();
+    if (!(m_remote_data.length() == 0 || !verify_checksum(m_remote_data))) {
+        m_telemetry_data = parse_remote_data(m_remote_data);
+    }
+    send_data();
 }
 
 bool Transceiver::verify_checksum(const String& data) {
@@ -39,37 +41,17 @@ bool Transceiver::verify_checksum(const String& data) {
     return true;
 }
 
-RemoteData Transceiver::parse_remote_data(const String& data) {
+TelemetryData Transceiver::parse_remote_data(const String& data) {
     JsonDocument m_json_data;
     deserializeJson(m_json_data, data);
     // Extract the bitmask from the JSON document
     uint32_t bitmask = m_json_data["b"];
-    RemoteData remote_data;
+    TelemetryData remote_data;
     // Unpack the bitmask
-    remote_data.throttle = (((bitmask >> 0) & 0x1FF) / 2.555) - 100;  // 9 bits for throttle
-    remote_data.steering = (((bitmask >> 9) & 0x1FF) / 2.555) - 100;  // 9 bits for steering
-    // compensate for resolution loss in the remote
-    if (abs(remote_data.throttle) < 0.3) {
-        remote_data.throttle = 0;
-    }
-    if (abs(remote_data.steering) < 0.3) {
-        remote_data.steering = 0;
-    }
-    remote_data.left_arrow = (bitmask >> 18) & 0x1;
-    remote_data.right_arrow = (bitmask >> 19) & 0x1;
-    remote_data.up_arrow = (bitmask >> 20) & 0x1;
-    remote_data.down_arrow = (bitmask >> 21) & 0x1;
-    remote_data.sel = (bitmask >> 22) & 0x1;
-    remote_data.ch = (bitmask >> 23) & 0x1;
-    remote_data.plus = (bitmask >> 24) & 0x1;
-    remote_data.minus = (bitmask >> 25) & 0x1;
-    remote_data.left_trim_l = (bitmask >> 26) & 0x1;
-    remote_data.left_trim_r = (bitmask >> 27) & 0x1;
-    remote_data.right_trim_l = (bitmask >> 28) & 0x1;
-    remote_data.right_trim_r = (bitmask >> 29) & 0x1;
-    remote_data.edge_switch = (bitmask >> 30) & 0x1;
-    remote_data.bottom_switch = (bitmask >> 31) & 0x1;
-    remote_data.new_data = true;
+    remote_data.arm_state = (bitmask >> 0) & 0x1;
+    remote_data.steering_mode = (bitmask >> 1) & 0x3;
+    remote_data.drive_mode = (bitmask >> 3) & 0x3;
+    remote_data.battery_status = (bitmask >> 5) & 0x3;
     return remote_data;
 }
 
@@ -111,4 +93,3 @@ void Transceiver::send_data() {
     serializeJson(m_json_data, json);
     m_esp_now_handler->send_data(json);
 }
-String Transceiver::get_remote_data() { return m_remote_data; }
